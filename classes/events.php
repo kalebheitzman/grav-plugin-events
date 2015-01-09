a65b2bcdf25783f2d4f6a0a002386bd5000452bd
Kaleb Heitzman <kalebheitzman@gmail.com>
2015-01-09 04:23:45 -0500

    Added carbon library

--- classes/events.php
+++ classes/events.php
@@ -2,9 +2,12 @@
 
 namespace Grav\Plugin;
 
+require __DIR__.'/../vendor/autoload.php';
+
 use Grav\Common\Grav;
 use Grav\Common\GravTrait;
 use Grav\Common\Page\Collection;
+use \Carbon\Carbon;
 
 class Events
 {
@@ -16,28 +19,40 @@ class Events
 	protected $config;
 
 	/**
+	 * @var string Date format
+	 */
+	protected $dateFormat; 
+
+	/**
 	 * @var object Collection of events
 	 */ 
 	protected $events;
+
+	/**
+	 * @var object Carbon date 
+	 */
+	protected $now;
 
 	/**
 	 * Construct
 	 */ 
 	public function __construct()
 	{
+		// set the current date
+		$this->now = Carbon::now();
+		// get the config
 		$this->config = self::$grav['config'];
-
+		// date format
+		$this->dateFormat = $this->config->get('plugins.events.date_format');
 		/** @var Taxonomy $taxonomy_map */
 		$taxonomy_map = self::$grav['taxonomy'];
-
 		// get the plugin filters setting
 		$filters = (array) $this->config->get('plugins.events.filters');
 		$operator = $this->config->get('plugins.events.filter_combinator');
-
 		// create a new collection of all events
 		$collection = new Collection();
 		$collection->append($taxonomy_map->findTaxonomy($filters, $operator)->toArray());
-
+		// set events
 		$this->events = $collection;
 	}
 
@@ -46,9 +61,37 @@ class Events
 	 * 
 	 * @return object Collection of all events
 	 */
-	public function get()
+	public function getEvents($attrs = null)
 	{
-		return $this->events;
+		if (is_null($attrs)) {
+			return $this->events;			
+		}
+
+
+
+		return $this->events;			
+	}
+
+	/**
+	 * Get start of week
+	 * 
+	 * @return string DateTime
+	 */
+	public function startOfWeek()
+	{
+		$startDate = Carbon::parse('last monday');
+		return $startDate->format($this->dateFormat);
+	}
+
+	/**
+	 * Get end of week
+	 * 
+	 * @return string DateTime
+	 */ 
+	public function endOfWeek()
+	{
+		$endDate = Carbon::parse('next monday');
+		return $endDate->format($this->dateFormat);
 	}
 
 }
