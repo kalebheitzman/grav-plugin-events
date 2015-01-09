79998ef5b30f0700b089ff0d307b11bb9e3e69cd
Kaleb Heitzman <kalebheitzman@gmail.com>
2015-01-09 03:48:18 -0500

    overhaul of events plugin based on Grav 0.9.13

--- classes/events.php
+++ classes/events.php
@@ -11,78 +11,24 @@ class Events
 	use GravTrait;
 
 	/**
-	 * @var array Events collection
+	 * @var object Config
+	 */ 
+	protected $config;
+
+	/**
+	 * @var object Collection of events
 	 */ 
 	protected $events;
 
 	/**
-	 * @var Config $config
-	 */
-	protected $config;
-
-	/**
-	 * Class construct
-	 */
+	 * Construct
+	 */ 
 	public function __construct()
 	{
-		// get configuration
 		$this->config = self::$grav['config'];
 
-		// Dynamically add the needed taxonomy types to the taxonomies config
-		$event_taxonomies = array('type');
-		$taxonomy_config = array_merge((array)$this->config->get('site.taxonomies'), $event_taxonomies);
-		$this->config->set('site.taxonomies', $taxonomy_config);
-	}
-
-	/**
-	 * Find events based on options
-	 * 
-	 * @param array $patterns patterns to search
-	 * @param string $operator Operator
-	 * @return object Collection
-	 */
-	public function findEvents($patterns = null, $order = 'date', $operator = 'and')
-	{
-		// build the event listing
-	 	if (!$this->events) {
-	 		$this->build();
-	 	}
-
-        return $this->events;
-	}
-
-	/**
-	 * Build events list
-	 * 
-	 * @internal
-	 * @return object Collection
-	 */ 
-	protected function build()
-	{
 		/** @var Taxonomy $taxonomy_map */
 		$taxonomy_map = self::$grav['taxonomy'];
-	 	$pages = self::$grav['pages'];
-	 	$routes = $pages->routes();
-		ksort($routes);
-
-		// add event taxonomy to pages with event frontmatter
-		foreach($routes as $route => $path) {
-			$page = $pages->get($path);
-			// check if the page is routable
-			if ($page->routable()) {
-				// get the page header
-				$header = $page->header();
-				// check for event frontmatter 
-				if (isset($header->event)) {
-					// set type taxonomy to event
-					$taxonomy = $page->taxonomy();
-					if (!isset($taxonomy['type'])) {
-						$taxonomy['type'] = array($this->config->get('plugins.events.filters.type'));
-					}
-					$page->taxonomy($taxonomy);
-				}
-			}
-		}
 
 		// get the plugin filters setting
 		$filters = (array) $this->config->get('plugins.events.filters');
@@ -94,4 +40,15 @@ class Events
 
 		$this->events = $collection;
 	}
+
+	/**
+	 * Get all events
+	 * 
+	 * @return object Collection of all events
+	 */
+	public function get()
+	{
+		return $this->events;
+	}
+
 }
--- events.php
+++ events.php
@@ -2,7 +2,13 @@
 
 namespace Grav\Plugin;
 
-use \Grav\Common\Plugin;
+use Grav\Common\Plugin;
+use Grav\Common\Grav;
+use Grav\Common\Page\Collection;
+use Grav\Common\Page\Page;
+use Grav\Common\Debugger;
+use Grav\Common\Taxonomy;
+use RocketTheme\Toolbox\Event\Event;
 
 class EventsPlugin extends Plugin
 {
@@ -26,11 +32,16 @@ class EventsPlugin extends Plugin
 			return;
 		}
 
+		// Dynamically add the needed taxonomy types to the taxonomies config
+		$event_taxonomies = array('type', 'event_freq', 'event_repeat');
+		$taxonomy_config = array_merge((array)$this->config->get('site.taxonomies'), $event_taxonomies);
+		$this->config->set('site.taxonomies', $taxonomy_config);
+
 		$this->enable([
 			'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+			'onPageProcessed' => ['onPageProcessed', 0],
 			'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
 		]);
-
 	}
 
 	/**
@@ -42,6 +53,27 @@ class EventsPlugin extends Plugin
 	}
 
 	/**
+	 *	Process pages that have event frontmatter 
+	 */
+	public function onPageProcessed(Event $event)
+	{
+		// Get the page header
+		$page = $event['page'];
+		$header = $page->header();
+		$taxonomy = $page->taxonomy();
+
+		// check for event frontmatter 
+		if (isset($header->event)) {
+			// set the header date
+			$header->date = $header->event['start'];
+			$page->header($header);
+			// set the new event taxonomy
+			$taxonomy = $this->_eventFrontmatterToTaxonomy($page, $header);
+			$page->taxonomy($taxonomy);
+		}
+	}
+
+	/**
 	 * Set needed variables to display events
 	 */
 	public function onTwigSiteVariables()
@@ -50,6 +82,33 @@ class EventsPlugin extends Plugin
 
 		$twig = $this->grav['twig'];
 		$twig->twig_vars['events'] = new Events();
-	} 
+	}
+
+	/**
+	 * Convert event frontmatter to taxonomy
+	 * 
+	 * @param array $taxonomy Taxonomy
+	 * @param array $event Event details
+	 */ 
+	private function _eventFrontmatterToTaxonomy($page, $header)
+	{	
+		// event frontmatter
+		$event = $header->event;
+		// set type taxonomy to event
+		$taxonomy = $page->taxonomy();
+		if (!isset($taxonomy['type'])) {
+			$taxonomy['type'] = array($this->config->get('plugins.events.filters.type'));
+		}
+		// set event days that repeat
+		if (!isset($taxonomy['event_repeat']) && isset($event['repeat'])) {
+			$taxonomy['event_repeat'] = str_split($event['repeat']);
+		}
+		// set event frequency
+		if (!isset($taxonomy['event_freq']) && isset($event['freq'])) {
+			$taxonomy['event_freq'] = array($event['freq']);
+		}
+
+		return $taxonomy;
+	}
 
 }
