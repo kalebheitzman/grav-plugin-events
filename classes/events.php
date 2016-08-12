15ede1d5a475ce941ff1ce78ff9c038be78634b5
Kaleb Heitzman <kalebheitzman@gmail.com>
2016-08-12 17:46:59 -0400

    bumped version and added microformats

--- classes/events.php
+++ classes/events.php
@@ -13,7 +13,7 @@
  * @author     Kaleb Heitzman <kalebheitzman@gmail.com>
  * @copyright  2016 Kaleb Heitzman
  * @license    https://opensource.org/licenses/MIT MIT
- * @version    1.0.8
+ * @version    1.0.11
  * @link       https://github.com/kalebheitzman/grav-plugin-events
  * @since      1.0.0 Initial Release
  */
